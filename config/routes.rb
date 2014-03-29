d7f4441405f00f673dbc8085a41b03b3197cf34e
scott1743 <512981271@qq.com>
2014-03-29 18:33:59 +0800

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FdurLink::Application.routes.draw do
+  
+
+  resources :users do
+    resources :projects
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
