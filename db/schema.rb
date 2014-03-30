fdd2d9af18e5c3a0cc7b7b578081700644fe9118
qdeaqa <519421832@qq.com>
2014-03-30 14:46:53 +0800

    修改了 database socket  mac 运行正常

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329081458) do
+ActiveRecord::Schema.define(version: 20140329171704) do
 
   create_table "projects", force: true do |t|
     t.string   "content"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20140329081458) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
